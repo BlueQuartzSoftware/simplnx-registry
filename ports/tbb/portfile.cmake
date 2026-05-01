4dd8a2b973349943e23b58237a986785bfc6d7de
Jared Duffey <jared.duffey@bluequartz.net>
2026-05-01 15:22:53 -0400

    [tbb] Updated to version 2022.3.0
    Signed-off-by: Jared Duffey <jared.duffey@bluequartz.net>

--- ports/tbb/portfile.cmake
+++ ports/tbb/portfile.cmake
@@ -1,36 +1,44 @@
-if(NOT VCPKG_TARGET_IS_LINUX AND NOT VCPKG_TARGET_IS_OSX)
-  vcpkg_fail_port_install(ON_ARCH "arm" "arm64" ON_TARGET "uwp")
-endif()
+set(VCPKG_POLICY_MISMATCHED_NUMBER_OF_BINARIES enabled)
 
 vcpkg_from_github(
-  OUT_SOURCE_PATH SOURCE_PATH
-  REPO oneapi-src/oneTBB
-  REF v2021.4.0
-  SHA512 96da2bc351fd64dfa854f8e6cabc1c4e53af3d55760e99d6f83ad53779c727af333d13d6be0828ed70371cf796498d2063e9dd0e4b2f6451623bb5d28ccbf20b
-  HEAD_REF master
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO oneapi-src/oneTBB
+    REF "v${VERSION}"
+    SHA512 fdc50589785b1949ca1dd4429bbcedb180be4b8966da5243ddd1f8e9f97310dd603681e0bb83c1d6c2d3e27932f577ef6739e4e82f3c54af147f4d6d906b39f1
+    HEAD_REF master
+    PATCHES
 )
 
-vcpkg_configure_cmake(
-  SOURCE_PATH ${SOURCE_PATH}
-  PREFER_NINJA
-  OPTIONS
-    -DTBB_TEST=OFF
+vcpkg_check_features(
+    OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    INVERTED_FEATURES
+        hwloc TBB_DISABLE_HWLOC_AUTOMATIC_SEARCH)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        ${FEATURE_OPTIONS}
+        -DTBB_TEST=OFF
+        -DTBB_STRICT=OFF
 )
 
-vcpkg_install_cmake()
-
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/TBB)
-
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/TBB")
 vcpkg_copy_pdbs()
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/tbb)
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/share/doc"
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
+    # These are duplicate libraries provided on Windows -- users should use the tbb12 libraries instead
+    "${CURRENT_PACKAGES_DIR}/lib/tbb.lib"
+    "${CURRENT_PACKAGES_DIR}/debug/lib/tbb_debug.lib"
+)
 
-file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/tbb RENAME copyright)
+file(READ "${CURRENT_PACKAGES_DIR}/share/tbb/TBBConfig.cmake" _contents)
+file(WRITE "${CURRENT_PACKAGES_DIR}/share/tbb/TBBConfig.cmake" "
+include(CMakeFindDependencyMacro)
+find_dependency(Threads)
+${_contents}")
 
-file(RENAME "${CURRENT_PACKAGES_DIR}/share/doc/TBB/README.md" "${CURRENT_PACKAGES_DIR}/share/tbb/README.md")
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/pkgconfig")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/doc")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
